9561a20205fd92bed9012b4e6df27b2edc4489d6
varunkukade <varunkukade999@gmail.com>
2024-05-08 21:16:21 +0530

    Made chat bugs changes

--- src/components/BackButton/index.tsx
+++ src/components/BackButton/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { StyleProp, TouchableOpacity, ViewStyle } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { BackIcon } from '../../svg/BackIcon';
 import { useTheme } from 'react-native-paper';
 import type { MyMD3Theme } from '../../providers/amity-ui-kit-provider';
+
 interface IBackBtn {
   onPress?: () => any;
+  styles?: StyleProp<ViewStyle>;
 }
-export default function BackButton({ onPress }: IBackBtn) {
+export default function BackButton({ onPress, styles }: IBackBtn) {
   const theme = useTheme() as MyMD3Theme;
   const navigation = useNavigation<NativeStackNavigationProp<any>>();
   return (
@@ -18,6 +20,7 @@ export default function BackButton({ onPress }: IBackBtn) {
         navigation.goBack();
         onPress && onPress();
       }}
+      style={styles ? styles : {}}
     >
       <BackIcon color={theme.colors.base} />
     </TouchableOpacity>
